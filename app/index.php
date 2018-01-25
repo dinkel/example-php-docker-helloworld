10956efbfbf819754764848677ce6a643e23599a
Christian Luginbühl <dinkel@pimprecords.com>
2018-01-25 16:34:27 +0100

    Modification of user's name

--- app/index.php
+++ app/index.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
 
-    <title>APPUiO PHP Demo</title>
+    <title>dinkel's APPUiO PHP Demo</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -53,7 +53,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Hallo <?php echo 'APPUiO'?></h1>
+        <h1>Hallo <?php echo "dinkel's APPUiO" ?></h1>
         <p class="lead">APPUiO Example Dockerfile PHP</p>
       </div>
 
